695c1aad85b552f8e51b285baf50ffd3a95aa507
JCarlos Cori <juancarlos.cori@dharbor.com>
2017-06-28 13:44:44 -0400

    configured angular material

--- demo-material/src/app/app.module.ts
+++ demo-material/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
