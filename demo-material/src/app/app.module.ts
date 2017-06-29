63abf54a852b7e0fe578f028812aec4b9cec701b
Carlos <inf.juan.carlos@gmail.com>
2017-06-29 07:47:23 -0700

    add components

--- demo-material/src/app/app.module.ts
+++ demo-material/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
