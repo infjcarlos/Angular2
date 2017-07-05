b1e06111853bcdaafaa4b53a1335021b85226d37
Carlos <inf.juan.carlos@gmail.com>
2017-07-05 07:33:59 -0700

    adding routes

--- modulechat/frontend/src/app/app.module.ts
+++ modulechat/frontend/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'; 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: NavBarComponent },
+  { path: 'login',      component: LoginComponent },
+  /*{ path: 'forms',      component: FormsComponent },*/
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LoginComponent
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
