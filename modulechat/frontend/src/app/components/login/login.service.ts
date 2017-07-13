41b36955fcd9a7fff355edacc19f8ed0b270c084
infjcarlos <computer_programme@hotmail.com>
2017-07-13 13:58:26 -0400

    adding service to chat

---
+++ modulechat/frontend/src/app/components/login/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, Inject, forwardRef } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import { ActivatedRoute, Router } from '@angular/router';
+//import { ConstantsService} from './constants.service';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class LoginService {
+
+  constructor() { }
+
+}
